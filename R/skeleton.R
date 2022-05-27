690ccd7d3cdbdacff49f4377c69aeb0808eb9421
dennisgrobe <dennis.grobe@shinternet.ch>
2022-05-27 19:45:52 +0200

    update skeleton.R

--- R/skeleton.R
+++ R/skeleton.R
@@ -1,16 +1,16 @@
 # from https://github.com/rstudio/bookdown/blob/master/R/utils.R
-thesisdown_file <- function(...) {
-  system.file(..., package = 'thesisdown', mustWork = TRUE)
+zurichdown_file <- function(...) {
+  system.file(..., package = 'zurichdown', mustWork = TRUE)
 }
 
 # from https://github.com/rstudio/bookdown/blob/master/R/skeleton.R
-thesisdown_skeleton <- function(path = getwd()) {
+zurichdown_skeleton <- function(path = getwd()) {
 
   # ensure directory exists
 #  dir.create(path, recursive = TRUE, showWarnings = FALSE)
 
   # copy 'resources' folder to path
-  resources <- thesisdown_file('rstudio', 'templates', 'project', 'resources')
+  resources <- zurichdown_file('rstudio', 'templates', 'project', 'resources')
 
   R.utils::copyDirectory(from = resources,
                          to = path,
